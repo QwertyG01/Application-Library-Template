85400c38ae76cfc5fe757e4ef0d6bcb0d0b870ca
QwertyG01 <giovannicasiraghi01@gmail.com>
2022-06-17 23:20:54 +0200

    Working example with threejs

--- app/examples/second/code.ts
+++ app/examples/second/code.ts
@@ -1,1 +1,41 @@
-console.log("This is my second example");
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  StaticDrawUsage,
+  WebGLRenderer,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Select the canvas from the document
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const renderer = new WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, renderer.domElement);
+// document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+controls.update();
+
+function draw() {
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(draw);
